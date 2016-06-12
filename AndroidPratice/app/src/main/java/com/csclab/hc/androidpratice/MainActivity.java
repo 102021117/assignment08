7c9cb14874373c5d7f42f93f36ba71a75014415c
unknown <>
2016-06-12 20:15:51 +0800

    no message

---
+++ AndroidPratice/app/src/main/java/com/csclab/hc/androidpratice/MainActivity.java
@@ -0,0 +1,264 @@
+package com.csclab.hc.androidpratice;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.CharBuffer;
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+    /**
+     * Init Variable for Page 1
+     **/
+    EditText inputNumTxt1;
+    EditText inputNumTxt2;
+    EditText inputip;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+    Button btnok;
+    /**
+     * Init Variable for Page 2
+     **/
+    TextView textResult;
+
+    Button return_button;
+
+    Socket connectionToServer;
+    /**
+     * Init Variable
+     **/
+    String oper = "";
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    /**
+     * Called when the activity is first created.
+     */
+    class ClientThread extends Thread {
+        private BufferedReader reader;
+        public ClientThread(BufferedReader reader) {
+            this.reader = reader;
+        }
+        public void run() {
+            while(true) {
+                try {
+                    String line = this.reader.readLine();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private ClientThread connection;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /** Func() for setup page 1 **/
+        try {
+            jumpTOSocketLayout();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+    public void jumpTOSocketLayout() throws IOException {
+        setContentView(R.layout.socket_page);
+        inputip = (EditText) findViewById(R.id.ip);
+        btnok = (Button) findViewById(R.id.ok_button);
+        btnok.setOnClickListener(this);
+        this.connectionToServer = new Socket(String.valueOf(inputip), 2000);
+        if (btnok != null) {
+            //TODO: prepare button listener for return button
+            // HINT:
+            // mybutton.setOnClickListener(new View.OnClickListener(){
+            //      public void onClick(View v) {
+            //          // Something to do..
+            //      }
+            // }
+            btnok.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO
+                    jumpToMainLayout();
+                }
+
+            });
+        }
+    }
+    /**
+     * Function for page 1 setup
+     */
+    public void jumpToMainLayout() {
+        //TODO: Change layout to activity_main
+        // HINT: setContentView()
+        setContentView(R.layout.activity_main);
+
+        //TODO: Find and bind all elements(4 buttons 2 EditTexts)
+        // inputNumTxt1, inputNumTxt2
+        // btnAdd, btnSub, btnMult, btnDiv
+        inputNumTxt1 = (EditText) findViewById(R.id.etNum1);
+        inputNumTxt2 = (EditText) findViewById(R.id.etNum2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        //TODO: Set 4 buttons' listener
+        // HINT: myButton.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+    }
+
+    /**
+     * Function for onclick() implement
+     */
+    @Override
+    public void onClick(View v) {
+        float num1 = 0; // Store input num 1
+        float num2 = 0; // Store input num 2
+        float result = 0; // Store result after calculating
+
+        // check if the fields are empty
+        if (TextUtils.isEmpty(inputNumTxt1.getText().toString())
+                || TextUtils.isEmpty(inputNumTxt2.getText().toString())) {
+            return;
+        }
+
+        // read EditText and fill variables with numbers
+        num1 = Float.parseFloat(inputNumTxt1.getText().toString());
+        num2 = Float.parseFloat(inputNumTxt2.getText().toString());
+
+        // defines the button that has been clicked and performs the corresponding operation
+        // write operation into oper, we will use it later for output
+        //TODO: caculate result
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        // HINT:Using log.d to check your answer is correct before implement page turning
+        Log.d("debug", "ANS " + result);
+        //TODO: Pass the result String to jumpToResultLayout() and show the result at Result view
+        jumpToResultLayout(new String(num1 + " " + oper + " " + num2 + " = " + result));
+    }
+
+    public void jumpToResultLayout(String resultStr) {
+        setContentView(R.layout.result_page);
+
+        //TODO: Bind return_button and textResult form result view
+        // HINT: findViewById()
+        // HINT: Remember to give type
+        return_button = (Button) findViewById(R.id.return_button);
+        textResult = (TextView) findViewById(R.id.textResult);
+
+        if (textResult != null) {
+            //TODO: Set the result text
+            textResult.setText(resultStr);
+        }
+
+        if (return_button != null) {
+            //TODO: prepare button listener for return button
+            // HINT:
+            // mybutton.setOnClickListener(new View.OnClickListener(){
+            //      public void onClick(View v) {
+            //          // Something to do..
+            //      }
+            // }
+            return_button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // TODO
+                    jumpToMainLayout();
+                }
+
+            });
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new
+                    InputStreamReader(connectionToServer.getInputStream()));
+            reader.read(CharBuffer.wrap(resultStr));
+            this.connection = new ClientThread(reader);
+            this.connection.start();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.csclab.hc.androidpratice/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.csclab.hc.androidpratice/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
